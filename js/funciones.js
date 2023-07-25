2bffba5ed8a5a9f10df7769c7f02ae37ed67f462
oscardeperro <140541599+oscardeperro@users.noreply.github.com>
2023-07-25 11:17:15 -0600

    Add files via upload

---
+++ js/funciones.js
@@ -0,0 +1,26 @@
+function msj_salir(){
+//algorimo de la funcion//
+alert( "!vuelve pronte¡")
+
+}
+
+function invitar(){
+   var nombre;
+   var deseo;
+
+   nombre=prompt("Escribe tu nombre")
+   nombre=prompt("¿que te gusta ?")
+
+   alert("!hola¡ "+nombre+"te invitamos a comer"+deseo)
+
+}
+function mamada(){
+    var nombre;
+    var deseo;
+ 
+    nombre=prompt("Escribe tu nombre")
+    nombre=prompt("¿que te gusta ?")
+ 
+    alert("!hola¡ "+nombre+"te invitamos a comer"+deseo)
+ 
+ }
